77eac8a516636356f134fe4aa2a71b6a4dfe78a8
Olufemi Oladotun Daniel <olufemidaniels07@gmail.com>
2019-01-14 10:08:32 +0100

    Logic updated to a good level

--- src/components/Expenses.vue
+++ src/components/Expenses.vue
@@ -3,6 +3,15 @@
     <div class="row justify-content-center">
       <div class="col-md-6 mt-5" style="height : 10vh">
         <div class="holder">
+          <p class="conc"
+          v-if="max_expense_sum === feed_total"
+          >You spent more on Feeding</p>
+          <p class="conc"
+          v-if="max_expense_sum === transport_total"
+          >You spent more on Transportation</p>
+          <p class="conc"
+          v-if="max_expense_sum === Miscellaneous_tot"
+          >You spent more on Miscellaneous activities</p>
           <form @submit.prevent>
             <select v-model="category">
               <option
@@ -58,6 +67,9 @@
           </transition-group>
 
           <p>Kindly input the expenses for the week</p>
+          <p v-if="feed_tot > 1">You spent &#8358;{{feed_tot}} on Feeding</p>
+          <p v-if="transport_total > 1">You spent &#8358;{{transport_total}} on Transportation</p>
+          <p v-if="Miscellaneous_tot > 1">You spent &#8358;{{Miscellaneous_tot}} on Miscellaneous expenses</p>
         </div>
       </div>
     </div>
@@ -74,6 +86,10 @@ export default {
       time: "Morning",
       price: "",
       id: "",
+      feed_tot : 0,
+      Miscellaneous_tot: 0,
+      transport_total : 0,
+      max_expense_sum : Number,
       categories: [
         { name: "Feeding" },
         { name: "Transportation" },
@@ -93,13 +109,22 @@ export default {
       ],
       feeding: [{ price: 0 }],
       transportation: [{ price: 0 }],
-      Miscellaneous: [{ price: 0 }]
+      Miscellaneous: [{ price: 0 }],
+      sum_array : []
     };
   },
   methods: {
     addExpense() {
       if (this.category === "Feeding") {
         this.feeding.push({
+          price: this.price
+        });
+      }else if(this.category === "Transportation"){
+        this.transportation.push({
+          price: this.price
+        });
+      }else if(this.category === "Miscellaneous"){
+        this.Miscellaneous.push({
           price: this.price
         });
       }
@@ -117,32 +142,41 @@ export default {
       // console.log('I work');
     },
     print: function() {
-      let total = JSON.stringify(this.feeding); 
+      let total = JSON.stringify(this.expenses); 
       // this.feeding.price = this.feeding.price += this.feeding.price;
       console.log(total);
       console.log(this.feeding.price);
 
-      // function sum(obj){
-      //   var sum  = 0;
-      //   for (var el in obj){
-      //     if (obj.hasOwnProperty( el )){
-      //     sum += parseFloat(obj[el]);          
-      //   }
-      //   }
-      //   return sum;
-      // }
-
-      // var summed = sum(this.feeding);
-      // console.log("sum: " + summed);
 
       function sum(items, prop){
         return items.reduce(function (a, b){
           return a + b[prop];
         }, 0);
-      };
+      }
       let feed = this.feeding;
-      let tot = sum(feed, 'price');
-      console.log(tot);
+      this.feed_tot = sum(feed, 'price');
+      
+      console.log("feed_total :" + this.feed_tot);
+
+      let transport = this.transportation;
+      this.transport_total = sum(transport, 'price');
+      console.log("transport_total :" + this.transport_total);
+
+      let Miscellaneous = this.Miscellaneous;
+      this.Miscellaneous_tot = sum(Miscellaneous, 'price');
+      console.log("Miscellaneous_tot :" + this.Miscellaneous_tot);
+
+      this.sum_array.push(this.feed_tot, this.transport_total, this.Miscellaneous_tot);
+
+      let sum_total = JSON.stringify(this.sum_array);
+      console.log(sum_total);
+
+      function max_expense(sum_array){
+        return Math.max.apply(Math, sum_array);
+      }
+      console.log(max_expense(this.sum_array));
+this.max_expense_sum = max_expense(this.sum_array)
+      console.log("sum :" + this.max_expense_sum);
     }
   }
 };
@@ -150,4 +184,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
+.conc{
+  color: red;
+}
 </style>
